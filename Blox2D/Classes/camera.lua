93bebbc2936951ff5e620279f58b61e0c1e78bd8
SecreTPT <martimviseu@live.com.pt>
2021-10-10 15:25:42 +0100

    Create camera.lua

---
+++ Blox2D/Classes/camera.lua
@@ -0,0 +1,46 @@
+local Camera = {}
+Instance._Dictionary.Camera = Camera
+
+local Table = {
+    __setters = {},
+    __getters = {},
+}
+Camera._Table = Table
+setmetatable(Table, {__index = Instance._Table})
+setmetatable(Table.__setters, Instance.__setters_metatable)
+Camera.__setters_metatable = {__index = Table.__setters}
+setmetatable(Table.__getters, Instance.__getters_metatable)
+Camera.__getters_metatable = {__index = Table.__getters}
+
+function Table.__setters:Position(vector)
+    Check("Set(Position)", "table", vector, "vector")
+    if typeof(vector) == "Vector2" then
+        self:Set("Position", vector)
+    end
+end
+function Table.__setters:Rotation(value)
+    Check("Set(Rotation)", "number", value, "value")
+    self:Set("Rotation", value)
+end
+
+local metatable = {
+    __index = function (tbl, key)
+        if Table.__getters[key] then
+            return Table.__getters[key]()
+        end
+        return rawget(tbl, "_"..key) or Table[key] or tbl:FindFirstChild(key)
+    end,
+    __newindex = Instance.metatable.__newindex,
+    __tostring = Instance.metatable.__tostring
+}
+
+Camera.new = function(instance)
+    setmetatable(instance, metatable)
+    table.insert(rawget(instance, "_ClassNames"), "Camera")
+    instance.Name = "Camera"
+    rawset(instance, "_Position", Vector2.new())
+    rawset(instance, "_Rotation", 0)
+    rawset(instance, "_ViewportSize", Vector2.new(love.graphics.getDimensions()))
+end
+
+return Camera
