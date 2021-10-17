7ecd790736be47eb33d33b917862333768c4058e
SecreTPT <martimviseu@live.com.pt>
2021-10-17 01:31:27 +0100

    KeyCode Enums

--- Blox2D/DataTypes/enum.lua
+++ Blox2D/DataTypes/enum.lua
@@ -2,7 +2,14 @@ local EnumType = require("Blox2D.DataTypes.enumtype")
 local EnumItem = require("Blox2D.DataTypes.enumitem")
 
 local EnumTypes = {
-    PartShape = EnumType.new(EnumItem.new("Rectangle", 1), EnumItem.new("Sphere"))
+    PartShape = EnumType.new(EnumItem.new("Rectangle", 1), EnumItem.new("Sphere", 0)),
+    KeyCode = EnumType.new(
+        EnumItem.new("Unknown", 0),
+        EnumItem.new("Backspace", 8),
+        EnumItem.new("Tab", 9),
+        EnumItem.new("Clear", 12),
+        EnumItem.new("Return", 13)
+    )
 }
 
 local Enum = setmetatable({}, {
