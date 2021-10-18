ffd4966d8407c0c72ddaca9eba1858af3591698d
SecreTPT <martimviseu@live.com.pt>
2021-10-19 00:16:54 +0100

    Update userinputservice.lua

--- Blox2D/Classes/userinputservice.lua
+++ Blox2D/Classes/userinputservice.lua
@@ -1,86 +1,10 @@
 local Class, Table, getters, setters, newFunc = _Inherit(Instance, "UserInputService")
 
 
-local keyToKeyCodeName = {
-    [string.char(34)] = "QuotedDouble",
-    ["#"] = "Hash",
-    ["$"] = "Dollar",
-    ["%"] = "Percent",
-    ["&"] = "Ampersand",
-    [string.char(39)] = "Quote",
-    ["("] = "LeftParenthesis",
-    [")"] = "RightParenthesis",
-    ["*"] = "Asterisk",
-    ["+"] = "Plus",
-    [","] = "Comma",
-    ["-"] = "Minus",
-    ["."] = "Period",
-    ["/"] = "Slash",
-    ["0"] = "Zero",
-    ["1"] = "One",
-    ["2"] = "Two",
-    ["3"] = "Three",
-    ["4"] = "Four",
-    ["5"] = "Five",
-    ["6"] = "Six",
-    ["7"] = "Seven",
-    ["8"] = "Eight",
-    ["9"] = "Nine",
-    [":"] = "Colon",
-    [";"] = "Semicolon",
-    ["<"] = "LessThan",
-    ["="] = "Equals",
-    [">"] = "GreaterThan",
-    ["?"] = "Question",
-    ["@"] = "At",
-    ["["] = "LeftBracket",
-    [string.char(92)] = "BackSlash",
-    ["]"] = "RightBracket",
-    ["^"] = "Caret",
-    ["_"] = "Underscore",
-    ["`"] = "Backquote",
-    ["{"] = "LeftCurly",
-    ["|"] = "Pipe",
-    ["}"] = "RightCurly",
-    ["~"] = "Tilde",
-    ["kp0"] = "KeyPadZero",
-    ["kp1"] = "KeyPadOne",
-    ["kp2"] = "KeyPadTwo",
-    ["kp3"] = "KeyPadThree",
-    ["kp4"] = "KeyPadFour",
-    ["kp5"] = "KeyPadFive",
-    ["kp6"] = "KeyPadSix",
-    ["kp7"] = "KeyPadSeven",
-    ["kp8"] = "KeyPadEight",
-    ["kp9"] = "KeyPadNine",
-    ["kp."] = "KeyPadPeriod",
-    ["kp/"] = "KeyPadDivide",
-    ["kp*"] = "KeyPadMultiply",
-    ["kp-"] = "KeyPadMinus",
-    ["kp+"] = "KeyPad+",
-    ["kpenter"] = "KeyPadEnter",
-    ["kp="] = "KeyPadEquals",
-    ["pageup"] = "PageUp",
-    ["pagedown"] = "PageDown",
-    ["lshift"] = "LeftShift",
-    ["rshift"] = "RightShift",
-    ["lalt"] = "LeftAlt",
-    ["ralt"] = "RightAlt",
-    ["lctrl"] = "LeftControl",
-    ["rctrl"] = "RightControl",
-    ["capslock"] = "CapsLock",
-    ["numlock"] = "NumLock",
-    ["scrollock"] = "ScrollLock",
-    ["lgui"] = "LeftSuper",
-    ["rgui"] = "RightSuper",
-    ["printscreen"] = "Print",
-    ["sysreq"] = "SysReq",
-    ["currencyunit"] = "Euro",
-    
-}
 
 
-function Table:_NewInputObject(key)
+
+--[[function Table:_NewInputObject(key)
     local keyCode = Enum.KeyCode[key] or keyToKeyCodeName[key]
     print(key, keyCode)
     if keyCode == nil then
@@ -89,7 +13,7 @@ function Table:_NewInputObject(key)
     end
     local inputObject = Instance.new("InputObject")
     inputObject.KeyCode = keyCode
-end
+end--]]
 
 Class.new = function ()
     local instance = newFunc()
