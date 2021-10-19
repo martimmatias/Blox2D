17a99ca78d1693908115b3e603fadf1e7dd92f21
SecreTPT <martimviseu@live.com.pt>
2021-10-19 23:18:18 +0100

    Update players.lua

--- Blox2D/Classes/players.lua
+++ Blox2D/Classes/players.lua
@@ -1,7 +1,28 @@
 local Class, Table, getters, setters, newFunc = _Inherit(Instance, "Players")
 
+function Table:GetPlayers()
+    local players = {}
+    for _, player in pairs(self._Children) do
+        if player:IsA("Player") then
+            table.insert(players, player)
+        end
+    end
+    return players
+end
+
+function Table:CreateLocalPlayer()
+    local player = Instance.new("Player")
+    player.Parent = self
+    rawset(self, "_LocalPlayer", player)
+    return player
+end
+
 Class.new = function ()
-    return newFunc()
+    local instance = newFunc()
+    rawset(instance, "PlayerAdded", ScriptSignal.new())
+    rawset(instance, "PlayerRemoving", ScriptSignal.new())
+    rawset(instance, "_RespawnTime", 5)
+    return instance
 end
 
 return Class
