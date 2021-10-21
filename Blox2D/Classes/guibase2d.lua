f58a505dbca5b70532b9f7d303da82f12b263a39
SecreTPT <martimviseu@live.com.pt>
2021-10-21 18:09:40 +0100

    Update guibase2d.lua

--- Blox2D/Classes/guibase2d.lua
+++ Blox2D/Classes/guibase2d.lua
@@ -1,6 +1,6 @@
 local Class, Table, getters, setters, newFunc = _Inherit(Instance, "GuiBase2d")
 
-function setters:AutoLocalize()
+function setters:AutoLocalize(value)
     Check("Set(AutoLocalize)", "boolean", value, "value")
     self:Set("AutoLocalize", value)
 end
@@ -13,7 +13,7 @@ function getters:AbsoluteSize()
     return Vector2.new(self._AbsoluteSize.X, self._AbsoluteSize.Y)
 end
 
-GuiBase2d.new = function()
+Class.new = function()
     local instance = newFunc()
     rawset(instance, "_AbsoluteSize", Vector2.new())
     rawset(instance, "_AbsolutePosition", Vector2.new())
