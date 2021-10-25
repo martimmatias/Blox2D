e639913b650f39ed458e8b4c7efbad19349b08ba
Martim Matias <martimviseu@live.com.pt>
2021-10-25 15:50:33 +0100

    Update baseplayergui.lua

--- Blox2D/Classes/baseplayergui.lua
+++ Blox2D/Classes/baseplayergui.lua
@@ -1,5 +1,9 @@
 local Class, Table, getters, setters, newFunc = _Inherit(Instance, "BasePlayerGui", Instance.__getters_metatable, Instance.__setters_metatable)
 
+function Table:GetGuiObjectsAtPosition(x, y)
+
+end
+
 Class.new = function ()
     return newFunc()
 end
